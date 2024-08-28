69a1b10643f7fce3f5739a4946dd44956555e956
D0ckDrew <DockDrew@yandex.ru>
2024-08-28 19:25:49 +0300

    refactor: отрефакторил скрипт создания таблиц в БД

--- src/main/resources/db/changelog/01_create_tables.sql
+++ src/main/resources/db/changelog/01_create_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE client
 
 CREATE TABLE phone
 (
-    client_id         VARCHAR(255)    NOT NULL,
+    client_id       VARCHAR(255)    NOT NULL,
     phone_number    VARCHAR(255)    NOT NULL,
     PRIMARY KEY (client_id, phone_number),
     CONSTRAINT fk_phone_client FOREIGN KEY (client_id) REFERENCES client (id)
@@ -17,7 +17,7 @@ CREATE TABLE phone
 
 CREATE TABLE email
 (
-    client_id         VARCHAR(255)    NOT NULL,
+    client_id       VARCHAR(255)    NOT NULL,
     email_address   VARCHAR(255)    NOT NULL,
     PRIMARY KEY (client_id, email_address),
     CONSTRAINT fk_email_client FOREIGN KEY (client_id) REFERENCES client (id)
